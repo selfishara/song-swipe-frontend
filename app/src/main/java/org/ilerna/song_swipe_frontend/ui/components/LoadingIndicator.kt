340ffb9a9d85b6406b7c1c44b5f62d99244b8b21
JonathanVillamizar <jonathanvillamizar01@alumnes.ilerna.com>
2025-11-26 17:42:01 +0100

    Changes according code review

--- app/src/main/java/org/ilerna/song_swipe_frontend/ui/components/LoadingIndicator.kt
+++ app/src/main/java/org/ilerna/song_swipe_frontend/ui/components/LoadingIndicator.kt
@@ -1,6 +1,5 @@
 package org.ilerna.song_swipe_frontend.ui.components
 
-
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
