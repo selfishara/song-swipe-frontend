250ffe9bfdb5f3a58bda4394cff225ec134a75c4
Alfre-dev2004 <alfredonoriega@alumnes.ilerna.com>
2026-01-21 19:08:26 +0100

    Creacion de los model de Track 2

--- app/src/main/java/org/ilerna/song_swipe_frontend/domain/model/Album.kt
+++ app/src/main/java/org/ilerna/song_swipe_frontend/domain/model/Album.kt
@@ -2,16 +2,14 @@ package org.ilerna.song_swipe_frontend.domain.model
 
 data class Album(
     val album_type: String,
-    val artists: List<ArtistX>,
+    val artists: List<Artist>,
     val available_markets: List<String>,
-    val external_urls: ExternalUrlsXXX,
     val href: String,
     val id: String,
     val images: List<Image>,
     val name: String,
     val release_date: String,
     val release_date_precision: String,
-    val restrictions: RestrictionsX,
     val total_tracks: Int,
     val type: String,
     val uri: String
--- app/src/main/java/org/ilerna/song_swipe_frontend/domain/model/Track.kt
+++ app/src/main/java/org/ilerna/song_swipe_frontend/domain/model/Track.kt
@@ -1,17 +1,13 @@
 package org.ilerna.song_swipe_frontend.domain.model
 
-data class Tracks(
+data class Track(
     val album: Album,
     val artists: List<Artist>,
     val duration_ms: Int,
-    val explicit: Boolean,
-    val href: String,
     val id: String,
     val is_playable: Boolean,
     val name: String,
-    val popularity: Int,
     val preview_url: String,
-    val track_number: Int,
     val type: String,
     val uri: String
 )
