8d5aead4852aabfad264f548126d27cac9e35d2a
biel-rhlm <bielrari@alumnes.ilerna.com>
2026-01-28 19:20:50 +0100

    todo el code hecho

--- app/src/main/java/org/ilerna/song_swipe_frontend/core/analytics/AnalyticsEvents.kt
+++ app/src/main/java/org/ilerna/song_swipe_frontend/core/analytics/AnalyticsEvents.kt
@@ -6,7 +6,6 @@ object AnalyticsEvents {
     const val SPOTIFY_LOGIN_START = "spotify_login_start"
     const val SPOTIFY_LOGIN_SUCCESS = "spotify_login_success"
     const val SPOTIFY_LOGIN_ERROR = "spotify_login_error"
-
     // Params
     const val ERROR_MESSAGE = "error_message"
 }
